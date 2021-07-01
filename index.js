7855329e3d260559507f6af8e4a1327e848d29ce
Joshua Hall <Scout420.4@gmail.com>
2021-06-30 17:20:48 -0700

    Identifiers, Model Run through identifier list Check content with model

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("@tensorflow/tfjs");
+const toxicity = require("@tensorflow-models/toxicity");
 
 const fs = require("fs");
 const Discord = require("discord.js")
@@ -8,6 +9,9 @@ const bot = new Discord.Client();
 const tokenDoc = JSON.parse(fs.readFileSync(__dirname + "/token.json"));
 const token = tokenDoc.token;
 
+//Identifier List
+const idList = JSON.parse(fs.readFileSync(__dirname + "/identifierList.json")).identifiers;
+
 bot.on("ready", ()=>
 {
     console.log("Ready to censor the public!");
@@ -15,11 +19,30 @@ bot.on("ready", ()=>
 
 bot.on("message", (msg)=>
 {
+    //Check All Identifiers In Message
     let identified = false;
+    for(const item of idList)
+    {
+        if(msg.content.includes(item))
+        {
+            identified = true;
+            break;
+        }
+    }
 
     if(identified)
     {
-        
+        const threshold = 7.5;
+
+        toxicity.load(threshold).then(model => 
+        {
+            const input = msg.content;
+
+            model.classify(input).then(predictions =>
+            {
+                console.log(predictions);
+            });
+        });
     }
 })
 bot.login(token);
